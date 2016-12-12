aad1309f1a2d9d8699eb1ab8d1d5cce50e6ce368
thomas danner <thmsdnnr@gmail.com>
2016-12-12 14:39:10 +0000

    first working commit

--- server.js
+++ server.js
@@ -1,6 +1,69 @@
-var express=require('express');
-var app=express();
-app.get('/', function(req,res) {
-res.send('hello world');
+var mongo=require('mongodb').MongoClient;
+var http=require('http');
+var io=require('socket.io')(http);
+var dbAddress='urlshort';
+var db_url='mongodb://localhost:27017/';
+
+//URL shortener code courtesy of github.com/delight-im/ShortURL
+var _alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_',
+_base = _alphabet.length;
+
+function encode(number) { //convert number to base-62
+var _alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+if (number===0) {return 0;}
+var a = [];
+while (number > 0) {
+a.push(_alphabet[number%62]);
+number = Math.floor(number / 62);
+}
+return a.join('');
+}
+
+function addUrl(url,callB) {
+mongo.connect(db_url, function(err, db){
+if (!err) {
+db.collection('addrz').findOne({'url':url}, function(err,data) {
+if (data==null) {
+db.collection('addrz').save({url:url,shrt:null},function(err, data){
+var encID=encode(String((data['ops'][0]['_id'])).replace(/[A-Za-z]+/g,'')); //remove spaces
+db.collection('addrz').update({url:url},{$set:{shrt:encID}});
+callB(encID);
+}); }
+else { // we already have the shortened URL
+callB(data.shrt);
+}
 });
+}}
+);}
+
+var express=require('express');
+var path=require('path');
+var bodyParser=require('body-parser');
+var validURL=require('valid-url');
+var app=express();
+app.use(express.static('/'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/:addy',function(req,res) { 
+mongo.connect(db_url, function(err, db) { if(!err) {
+db.collection('addrz').findOne({'shrt':req.params.addy}, function(err,data) {
+if(data==null){res.send(req.params.addy+ ' is not a valid shortened URL. Please try again.');}
+else {
+console.log(data.url);
+res.redirect(data.url);
+res.end();}
+});
+}});});
+
+app.post('/submit', function(req,res) {
+if (validURL.isUri(req.body.url)) {
+var output='Your shortened URL is https://fcc-urlshortener-thmsdnnr.c9users.io/';
+addUrl(req.body.url,function(encID){res.send(output+encID+'. looks good brah');});}
+else { //invalid URL...add some kind of client-side alert
+res.redirect('/'); }
+//res.end();
+//}
+});
+app.get('*', function(req,res){res.sendFile(path.join(__dirname, 'index.html'));});
 app.listen(process.env.PORT||8080,function(){console.log('sup brah im listening brah');});
